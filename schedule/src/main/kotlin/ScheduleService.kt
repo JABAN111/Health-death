9d4ff30dde335e92402b78aac6e2f3573800ab48
jaba <368601@edu.itmo.ru>
2025-03-16 15:29:31 +0300

    fix dir name

--- schedule/src/main/kotlin/ScheduleService.kt
+++ schedule/src/main/kotlin/ScheduleService.kt
@@ -1,8 +1,8 @@
 package mobile.schedule
 
 import com.google.protobuf.Empty
-import mobile.train.grpc.PingResponse
-import mobile.train.grpc.ScheduleServiceGrpcKt
+import mobile.schedule.grpc.PingResponse
+import mobile.schedule.grpc.ScheduleServiceGrpcKt
 
 class ScheduleService : ScheduleServiceGrpcKt.ScheduleServiceCoroutineImplBase() {
     override suspend fun ping(request: Empty): PingResponse {
