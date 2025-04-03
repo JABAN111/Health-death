31c46b8d15d3c1a013454e0f272e80ba7e5b1b3f
RAZRULETEL <qwerty.qwertlain@gmail.com>
2025-04-03 05:01:32 +0300

    Add app colors

--- android-app/app/src/main/kotlin/mobile/app/ui/theme/Theme.kt
+++ android-app/app/src/main/kotlin/mobile/app/ui/theme/Theme.kt
@@ -10,16 +10,16 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = mainGreen,
+    secondary = mainLightGreen,
+    tertiary = accentBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,7 +45,7 @@ fun HealthdeathTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
