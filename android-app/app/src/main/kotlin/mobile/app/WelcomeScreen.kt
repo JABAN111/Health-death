1e16b0a827fb2e606dd333e7fe3f09eda41a1eae
RAZRULETEL <qwerty.qwertlain@gmail.com>
2025-04-08 15:03:59 +0300

    Optimize imports

--- android-app/app/src/main/kotlin/mobile/app/WelcomeScreen.kt
+++ android-app/app/src/main/kotlin/mobile/app/WelcomeScreen.kt
@@ -1,8 +1,6 @@
 package mobile.app
 
-import android.widget.LinearLayout
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,7 +10,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -22,7 +19,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.navigation.NavController
 import mobile.app.ui.theme.mainGreen
 
 
