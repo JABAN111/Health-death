5d1775e224639e4cbeeb29c1f0a8181670f94f64
jaba <368601@edu.itmo.ru>
2025-05-03 13:06:42 +0300

    add sample

--- log/src/main/kotlin/logger/RedisSubscriber.kt
+++ log/src/main/kotlin/logger/RedisSubscriber.kt
@@ -21,6 +21,19 @@ class RedisSubscriber(
         }, channel)
     }
 
+
+    /**
+     * TODO Комментарий удалить, он скорее для Кирилла, который собирался бд делать)
+     * Как выглядят примерно сообщения
+     * curl localhost:23200/api/ping
+     * {"pingResults":["Diary Service: Pong","Log Service: Pong","Schedule Service: Pong!","Train Service: Pong","User Service: Pong"]}%
+     * В логе у микросервиса log:
+     *
+     *      звуки эмуляции бд: logger.model.Log@59585b4d
+     *      2025-05-03T10:02:40.412449506Z Получено сообщение на канале info: {"message":"pinged: [Diary Service: Pong, Log Service: Pong, Schedule Service: Pong!, Train Service: Pong, User Service: Pong]","time":"10:02:40.405903089"}
+     *
+     *
+     */
     private fun handleMessage(channel: String, message: String) {
         // TODO здесь СИНХРОННАЯ логика для сохранения в постгрю нужна
         //  попытка втащить сюда корутину для оптимизации потенциально черевата
